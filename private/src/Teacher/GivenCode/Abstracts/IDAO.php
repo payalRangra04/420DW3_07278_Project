917a5e5bbc41e7685a3a9421acd3f2d5098664aa
Marc-Eric Boury <meboury@hotmail.com>
2024-03-17 19:17:26 -0400

    Added IDAO interface documentation + disabled spellcheck inspection (annoying)

--- private/src/Teacher/GivenCode/Abstracts/IDAO.php
+++ private/src/Teacher/GivenCode/Abstracts/IDAO.php
@@ -12,21 +12,69 @@ declare(strict_types=1);
 namespace Teacher\GivenCode\Abstracts;
 
 /**
- * TODO: Interface documentation
+ * Interface for DAO-type objects enforcing basic database operations methods.
  *
  * @author Marc-Eric Boury
  * @since  2024-03-16
  */
 interface IDAO {
     
+    /**
+     * Retrieves a record of a certain DTO entity from the database and returns
+     * an appropriate DTO object instance.
+     *
+     * @param int $id The identifier value of the record to obtain.
+     * @return AbstractDTO|null The created object DTO instance or null if no record was found for the specified id.
+     *
+     * @author Marc-Eric Boury
+     * @since  2024-03-17
+     */
     public function getById(int $id) : ?AbstractDTO;
     
+    /**
+     * Creates a record for a certain DTO entity in the database.
+     * Returns an updated appropriate DTO object instance.
+     *
+     * @param AbstractDTO $dto The {@see AbstractDTO} instance to create a record of.
+     * @return AbstractDTO An updated {@see AbstractDTO} instance.
+     *
+     * @author Marc-Eric Boury
+     * @since  2024-03-17
+     */
     public function create(AbstractDTO $dto) : AbstractDTO;
     
+    /**
+     * Updates the record of a certain DTO entity in the database.
+     * Returns an updated appropriate DTO object instance.
+     *
+     * @param AbstractDTO $dto The {@see AbstractDTO} instance to update the record of.
+     * @return AbstractDTO An updated {@see AbstractDTO} instance.
+     *
+     * @author Marc-Eric Boury
+     * @since  2024-03-17
+     */
     public function update(AbstractDTO $dto) : AbstractDTO;
     
+    /**
+     * Deletes the record of a certain DTO entity in the database.
+     *
+     * @param AbstractDTO $dto The {@see AbstractDTO} instance to delete the record of.
+     * @return void
+     *
+     * @author Marc-Eric Boury
+     * @since  2024-03-17
+     */
     public function delete(AbstractDTO $dto) : void;
     
+    /**
+     * Deletes the record of a certain DTO entity in the database based on its identifier.
+     *
+     * @param int $id The identifier of the DTO entity to delete
+     * @return void
+     *
+     * @author Marc-Eric Boury
+     * @since  2024-03-17
+     */
     public function deleteById(int $id) : void;
     
 }
