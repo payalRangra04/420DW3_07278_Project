7faa3f02f18023fcdae86608b167a3f16508d44f
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-03-21 09:31:32 -0400

    Added two empty DTO classes as examples

---
+++ private/src/Teacher/Examples/DTOs/BookDTO.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * 420DW3_07278_Project BookDTO.php
+ * 
+ * @author Marc-Eric Boury (MEbou)
+ * @since 2024-03-21
+ * (c) Copyright 2024 Marc-Eric Boury 
+ */
+
+namespace Teacher\Examples\DTOs;
+
+use Teacher\GivenCode\Abstracts\AbstractDTO;
+
+/**
+ * TODO: Class documentation
+ *
+ * @author Marc-Eric Boury
+ * @since  2024-03-21
+ */
+class BookDTO extends AbstractDTO {
+    
+    /**
+     * @inheritDoc
+     */
+    public function getDatabaseTableName() : string {
+        // TODO: Implement getDatabaseTableName() method.
+    }
+}
